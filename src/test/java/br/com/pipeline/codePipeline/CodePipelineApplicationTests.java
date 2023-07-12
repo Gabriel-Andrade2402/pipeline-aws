07765a9cdc40010dfc8f9048b5c2634324b4d923
gabriel.silva <gabriel.silva@singlepoint.com.br>
2023-07-11 22:51:45 -0300

    version 1.1

---
+++ src/test/java/br/com/pipeline/codePipeline/CodePipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pipeline.codePipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodePipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
